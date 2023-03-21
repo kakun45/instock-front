fdf0e9ac637673f312d4749ff1b8b908c8603e60
Benjahmin Koenigsberg <magnanimousplayer@gmail.com>
2023-03-21 17:55:18 -0400

    updates to inventory list search container

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
 import { Footer } from './components/Header/Footer/Footer';
+import { InventoryList } from './components/InventoryList/InventoryList';
 
 function App() {
   return (
-<Footer />
+<InventoryList />
   );
 }
 
--- src/components/InventoryList/InventoryList.js
+++ src/components/InventoryList/InventoryList.js
@@ -3,9 +3,11 @@ import './InventoryList.scss'
 
 export const InventoryList = () => {
   return (
-    <body>
+    <div className="page" >
       <div className="inventory__search-container">
-        <h2>Inventory</h2>
+
+        <h2 className="inventory__header" >Inventory</h2>
+        
         <div className="inventory__search-inner-container">
           <input
             className="inventory__search-bar"
@@ -15,6 +17,6 @@ export const InventoryList = () => {
           <button className="inventory__search-button">Add New Item</button>
         </div>
       </div>
-    </body>
+    </div>
   );
 }
