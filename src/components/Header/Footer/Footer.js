7cc3210eca46873b1db657def174d4f663a2ecbf
Benjahmin Koenigsberg <magnanimousplayer@gmail.com>
2023-03-21 14:53:27 -0400

    creates footer component

---
+++ src/components/Header/Footer/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import '/Footer.scss';
+
+export const Footer = () => {
+  return (
+    <>
+      <div className='footer__main-container' >
+        <footer className='footer__container' >
+          <p className='footer__copyright__text' >@InStock Inc. All Rights Reserved.</p>
+        </footer>
+      </div>
+    </>
+  );
+}
