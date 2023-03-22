e0b96079d8467bb75632a9f7f328f2d328bf67d1
Benjahmin Koenigsberg <magnanimousplayer@gmail.com>
2023-03-22 15:42:21 -0400

    initail draft of delete modal

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Header from "./components/Header/Header";
-import { Footer } from "./components/Footer/Footer";
+// import { Footer } from "./components/Footer/Footer";
 import { InventoryList } from "./components/InventoryList/InventoryList";
 import InventoryItemDetails from "./components/InventoryItemDetails/InventoryItemDetails";
+import DeleteInventoryModal from "./components/DeleteInventoryModal/DeleteInventoryModal";
 import "./App.scss";
-import Header from "../src/components/Header/Header";
 
 function App() {
   return (
     <BrowserRouter>
-      <Header />
+      {/* <Header /> */}
       <Routes>
         {/* TODO change element to reflect home page */}
         <Route path="/" element={<InventoryItemDetails />} />
         <Route path="/inventory/:itemId" element={<InventoryItemDetails />} />
         <Route path="/inventory" element={<InventoryList/>} />
+        <Route path="/delete" element={ <DeleteInventoryModal/> }/>
       </Routes>
     </BrowserRouter>
   );
