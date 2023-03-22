c30d5e52fac4a8103077287e1c675629421ee435
Marcus Anderson <marqui0688@gmail.com>
2023-03-22 14:52:35 -0400

    Make Header responsive

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Header from "./components/Header/Header";
+// import Header from "./components/Header/Header";
 import { Footer } from "./components/Footer/Footer";
 import { InventoryList } from "./components/InventoryList/InventoryList";
 import InventoryItemDetails from "./components/InventoryItemDetails/InventoryItemDetails";
@@ -14,7 +14,7 @@ function App() {
         {/* TODO change element to reflect home page */}
         <Route path="/" element={<InventoryItemDetails />} />
         <Route path="/inventory/:itemId" element={<InventoryItemDetails />} />
-        <Route path="/inventory" element={<InventoryList/>} />
+        <Route path="/inventory" element={<InventoryList />} />
       </Routes>
     </BrowserRouter>
   );
